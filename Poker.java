f0349229f867a1dae2cfc69048e93ffb2b09fac9
DBW-cifo <55205836+DBW-cifo@users.noreply.github.com>
2019-10-15 10:45:51 +0200

    Add files via upload

--- Poker.java
+++ Poker.java
@@ -3,43 +3,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Poker {
-	public ArrayList<Espanyola> cartas = new ArrayList<Espanyola>();
+	public ArrayList<Francesa> cartas = new ArrayList<Francesa>();
 	public ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
-	System.out.println("*** Faltan jugadores ***");
-	  
+	
+	/* if (jugadores.size()>=2) */
 	   void addJugador(Jugador j) {
 		  jugadores.add(j);
 	   }
-	
-}
-
-/*
-
-	if (jugadores.size()>=2) {}
-	public void addJugador(Jugador j; {
-		jugadores.add(j);
-    }
-	}
-		void addJugador(Jugador j) {
-			jugadores.add(j);
-	    }
-	
-	
-	
-}
-
-/*public class Poker {
-	
-	public ArrayList<Francesa> cartas = new ArrayList<Francesa>();
-	public ArrayList<Jugador> jugadores = new ArrayList<Jugador>() {
-	 
-	if (jugadores.size()>=2) {
-		void addJugador(Jugador j) {
-		    jugadores.add(j);	
-	    }
-	
+	/* }
 	else {
-		  
-	}
-	}
-}*/
+	   System.out.println("*** Faltan jugadores ***");	
+	}*/
+}
